124656521c9367160b94fc8aa35626a2223d236f
USER <USER@hb1030>
2016-06-23 16:33:39 +0900

    ddaerarse

---
+++ netflix/src/jongj/MoviePlayer.java
@@ -0,0 +1,14 @@
+package jongj;
+
+public class MoviePlayer {
+	private MovieInfo movie;
+
+	public MovieInfo getMovie() {
+		return movie;
+	}
+
+	public void setMovie(MovieInfo movie) {
+		this.movie = movie;
+	}
+	
+}
---
+++ netflix/src/jongj/MoviePlayerService.java
@@ -0,0 +1,6 @@
+package jongj;
+
+public interface MoviePlayerService {
+	public void playMovie(MovieInfo movie);
+	public void back();
+}
